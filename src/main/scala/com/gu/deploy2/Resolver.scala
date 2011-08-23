566071db547acbac1d793557a04534942a492907
Graham Tackley <graham.tackley@guardian.co.uk>
2011-08-23 15:05:17 +0100

    Minor tidy and removal of println

--- src/main/scala/com/gu/deploy2/Resolver.scala
+++ src/main/scala/com/gu/deploy2/Resolver.scala
@@ -4,12 +4,8 @@ package com.gu.deploy2
 
 object Resolver {
   def resolve(recipe: Recipe, deployinfo: List[Host]): List[Task] = {
-    val hosts = deployinfo.filter(host => !(recipe.roles & host.roles).isEmpty)
-
-    println("hosts = " + hosts)
-
     for {
-      host <- hosts
+      host <- deployinfo.filterNot(host => (recipe.roles & host.roles).isEmpty)
       action <- recipe.actions
       tasks <- action.resolve(host)
     } yield {
