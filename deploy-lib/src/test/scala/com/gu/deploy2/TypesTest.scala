dd9ba10ba8428b370e2a257fd0537af68a3aaddb
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2011-08-24 10:21:03 +0100

    Fix test failure caused by refactor of resolve method

--- deploy-lib/src/test/scala/com/gu/deploy2/TypesTest.scala
+++ deploy-lib/src/test/scala/com/gu/deploy2/TypesTest.scala
@@ -6,27 +6,11 @@ import org.scalatest.matchers.ShouldMatchers
 import java.text.spi.CollatorProvider
 
 class TypesTest extends FlatSpec with ShouldMatchers {
-  val jettyJson = """
-  {
-    "packages":{
-      "webapp":{ "type":"jetty-webapp", "roles":["jetty"]  }
-    },
-    "recipes":{
-      "webapp-only":{
-        "actions":["webapp.deploy"],
-      }
-    }
-  }
-"""
-  val jettyRole = Role("jetty")
-
-  val jettyHost = List(Host("the_host").role(jettyRole))
 
   it should "Jetty Type should have a deploy action" in {
-    val parsed = JsonReader.parse(jettyJson)
-    val jettyRecipe = parsed.recipes("webapp-only")
+    val jetty = new JettyWebappPackageType()
 
-    Resolver.resolve(jettyRecipe, jettyHost) should be (List(
+    jetty.deployWebapp("webapp", Host("host_name")) should be (List(
       BlockFirewallTask(),
       CopyFileTask("packages/webapp", "/jetty-apps/webapp/"),
       RestartAndWaitTask(),
