83a27820292c549fb52abb9df1615543c0913912
Graham Tackley <graham.tackley@guardian.co.uk>
2011-08-24 10:13:10 +0100

    Added teamcity test reporting plugin

---
+++ project/plugins/project/DeployPlugins.scala
@@ -0,0 +1,10 @@
+import sbt._
+
+object DeployPlugins extends Build {
+  lazy val plugins = Project("deploy-plugins", file("."))
+    .dependsOn(
+      uri("git://github.com/guardian/sbt-teamcity-test-reporting-plugin.git#1.1"),
+      uri("git://github.com/guardian/sbt-version-info-plugin.git#1.0"),
+      uri("git://github.com/guardian/sbt-dist-plugin.git#1.5")
+    )
+}
