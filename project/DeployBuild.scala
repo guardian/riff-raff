ed534f65ca54a45996cc610b5e1e69a0377078f3
Graham Tackley <graham@tackley.net>
2011-09-01 10:28:13 +0100

    Included the awesome sbt-io library
    This is a copy of the source, since the published
    version of this lib is for scala 2.8.1 and we use
    2.9.0-1.  
    
    See sbt-io/NOTICE for copyright and license info.

--- project/DeployBuild.scala
+++ project/DeployBuild.scala
@@ -3,11 +3,16 @@ import Keys._
 import Defaults._
 
 object DeployBuild extends Build {
-  lazy val root = Project("root", file(".")) aggregate (lib, cli)
+  lazy val root = Project("root", file(".")) aggregate (lib, cli, sbtIo)
+
+  // This project contains code extracted from sbt for io handling
+  // Once sbt moves to the same version of scala as us, we can use a binary
+  // dependency instead
+  lazy val sbtIo = deployProject("sbt-io")
 
   lazy val lib = deployProject("deploy-lib")
 
-  lazy val cli = deployProject("deploy-cli") dependsOn(lib)
+  lazy val cli = deployProject("deploy-cli") dependsOn(lib, sbtIo)
 
 
   def deployProject(name: String) = Project(name, file(name), settings = defaultSettings ++ deploySettings)
