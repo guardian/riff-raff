65db469c69d7de0a02de74827168f3187f2c4f14
Simon Hildrew <simon.hildrew@guardian.co.uk>
2012-08-07 13:09:13 +0100

    Add custom error page and handling
    This is mainly to try and get to the bottom of the weird Akka problem that appears after riff-raff has been up for a while.

--- riff-raff/app/Global.scala
+++ riff-raff/app/Global.scala
@@ -1,9 +1,12 @@
-import controllers.DeployLibrary
+import controllers.{Logging, DeployLibrary}
 import notification.IrcClient
+import play.mvc.Http.RequestHeader
+import play.mvc.Result
 import play.{Application, GlobalSettings}
 import utils.ScheduledAgent
+import play.api.mvc.Results.InternalServerError
 
-class Global extends GlobalSettings {
+class Global extends GlobalSettings with Logging {
   override def onStart(app: Application) {
     // initialise message sinks
     IrcClient.init()
@@ -14,5 +17,10 @@ class Global extends GlobalSettings {
     IrcClient.shutdown()
     DeployLibrary.shutdown()
     ScheduledAgent.shutdown()
+  }
+
+  override def onError(request: RequestHeader, t: Throwable) = {
+    log.error("Error whilst trying to serve request", t)
+    new Result() { def getWrappedResult = InternalServerError(views.html.errorPage(t)) }
   }
 }
