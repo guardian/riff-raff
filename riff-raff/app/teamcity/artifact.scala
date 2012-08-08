63135ef85cb32059ffe16aedb9b35d5f1e57903d
Simon Hildrew <simon.hildrew@guardian.co.uk>
2012-08-08 15:12:42 +0100

    Add pre-fetching and serverside caching for build IDs

--- riff-raff/app/teamcity/artifact.scala
+++ riff-raff/app/teamcity/artifact.scala
@@ -7,6 +7,7 @@ import utils.ScheduledAgent
 import akka.util.duration._
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
+import controllers.Logging
 
 trait Artifact {
   def location: URL
@@ -40,18 +41,26 @@ object TeamCityBuild {
 }
 case class TeamCityBuild(name: String, number: String, startDate: DateTime) extends Build
 
-object TeamCity extends BuildServer {
+object TeamCity extends BuildServer with Logging {
   val tcURL = Configuration.teamcity.serverURL
   object api {
     val projectList = "/guestAuth/app/rest/projects"
     val buildList = "/guestAuth/app/rest/builds/?locator=buildType:%s"
   }
 
-  val buildTypeAgent = ScheduledAgent[Seq[BuildType]](1 minute, 1 minute)(getRetrieveBuildTypes)
+  val buildTypeAgent = ScheduledAgent[Seq[BuildType]](1 second, 1 minute)(getRetrieveBuildTypes)
+  val buildAgent = ScheduledAgent[Map[BuildType,Seq[Build]]](20 seconds, 1 minute){
+    log.info("Updating successful TeamCity builds")
+    val result = getSuccessfulBuildMap(retrieveBuildTypes)
+    log.info("Finished updating successful builds (found %d builds)" format result.values.map(_.size).reduce(_+_))
+    result
+  }
 
   def retrieveBuildTypes = buildTypeAgent()
   def successfulBuilds(projectName: String): List[Build] = retrieveBuildTypes.filter(_.name == projectName).headOption
-    .map(successfulBuilds(_).toList).getOrElse(Nil)
+    .map(successfulBuildMap(_).toList).getOrElse(Nil)
+
+  def successfulBuildMap = buildAgent()
 
   private def getRetrieveBuildTypes: Seq[BuildType] = {
     val projectElements = XML.load(new URL(tcURL,api.projectList))
@@ -63,8 +72,14 @@ object TeamCity extends BuildServer {
     }
   }
 
-  def successfulBuilds(buildType: BuildType): Seq[Build] = {
-    val buildElements = XML.load(new URL(tcURL, api.buildList format buildType.id))
+  private def getSuccessfulBuildMap(buildTypes: Seq[BuildType]): Map[BuildType,Seq[Build]] = {
+    buildTypes.map(buildType => buildType -> getSuccessfulBuilds(buildType)).toMap
+  }
+
+  private def getSuccessfulBuilds(buildType: BuildType): Seq[Build] = {
+    val url = new URL(tcURL, api.buildList format buildType.id)
+    log.debug("Getting %s" format url.toString)
+    val buildElements = XML.load(url)
     (buildElements \ "build") filter { build => (build \ "@status").text == "SUCCESS" } map { build =>
       TeamCityBuild(buildType.name, build \ "@number" text, build \ "@startDate" text)
     }
