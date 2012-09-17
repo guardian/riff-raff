b809c059fbbb456507430210438839d27547a3ca
Simon Hildrew <simon.hildrew@guardian.co.uk>
2012-09-17 17:47:23 +0100

    Fix tests

--- magenta-lib/src/test/scala/magenta/json/DeployInfoTest.scala
+++ magenta-lib/src/test/scala/magenta/json/DeployInfoTest.scala
@@ -12,18 +12,18 @@ class DeployInfoTest  extends FlatSpec with ShouldMatchers {
 {"group":"b", "stage":"CODE", "app":"microapp-cache", "hostname":"machost51.dc-code.gnl"},
 {"group":"a", "stage":"QA", "app":"microapp-cache", "hostname":"machost01.dc-qa.gnl"}
   ],
-  "keys":[
-  {"app":"microapp-cache", "stage":"CODE", "accesskey":"AAA"},
-  {"app":"frontend-article", "stage":"CODE", "accesskey":"CCC"},
-  {"app":"frontend-.*", "stage":"CODE", "accesskey":"BBB"},
-  {"app":"frontend-gallery", "stage":"CODE", "accesskey":"SHADOWED"},
-  {"app":"microapp-cache", "stage":".*", "accesskey":"DDD"}
-  ]}"""
+  "data":{ "aws-keys": [
+  {"app":"microapp-cache", "stage":"CODE", "value":"AAA"},
+  {"app":"frontend-article", "stage":"CODE", "value":"CCC"},
+  {"app":"frontend-.*", "stage":"CODE", "value":"BBB"},
+  {"app":"frontend-gallery", "stage":"CODE", "value":"SHADOWED"},
+  {"app":"microapp-cache", "stage":".*", "value":"DDD"}
+  ]}}"""
 
   "json parser" should "parse deployinfo json" in {
     val parsed = DeployInfoJsonReader.parse(deployInfoSample)
     parsed.hosts.size should be (3)
-    parsed.keys.size should be (5)
+    parsed.data.values.map(_.size).reduce(_+_) should be (5)
 
     val host = parsed.hosts(0)
     host should be (Host("machost01.dc-code.gnl", Set(App("microapp-cache")), "CODE"))
@@ -42,26 +42,26 @@ class DeployInfoTest  extends FlatSpec with ShouldMatchers {
 
   it should "match an exact app and stage" in {
     val di = DeployInfoJsonReader.parse(deployInfoSample)
-    di.firstMatchingKey(App("microapp-cache"),"CODE") should be(Some(Key("microapp-cache","CODE","AAA",None)))
+    di.firstMatchingData("aws-keys",App("microapp-cache"),"CODE") should be(Some(Data("microapp-cache","CODE","AAA",None)))
   }
 
   it should "match on a regex" in {
     val di = DeployInfoJsonReader.parse(deployInfoSample)
-    di.firstMatchingKey(App("frontend-front"),"CODE") should be(Some(Key("frontend-.*","CODE","BBB",None)))
+    di.firstMatchingData("aws-keys",App("frontend-front"),"CODE") should be(Some(Data("frontend-.*","CODE","BBB",None)))
   }
 
   it should "match the first in the list" in {
     val di = DeployInfoJsonReader.parse(deployInfoSample)
-    di.firstMatchingKey(App("frontend-article"),"CODE") should be(Some(Key("frontend-article","CODE","CCC",None)))
-    di.firstMatchingKey(App("frontend-gallery"),"CODE") should be(Some(Key("frontend-.*","CODE","BBB",None)))
+    di.firstMatchingData("aws-keys",App("frontend-article"),"CODE") should be(Some(Data("frontend-article","CODE","CCC",None)))
+    di.firstMatchingData("aws-keys",App("frontend-gallery"),"CODE") should be(Some(Data("frontend-.*","CODE","BBB",None)))
   }
 
   it should "not match bigger app or stage names" in {
     val di = DeployInfoJsonReader.parse(deployInfoSample)
-    di.firstMatchingKey(App("frontend-article"),"CODE2") should be(None)
-    di.firstMatchingKey(App("frontend-article"),"NEWCODE") should be(None)
-    di.firstMatchingKey(App("new-microapp-cache"),"CODE") should be(None)
-    di.firstMatchingKey(App("microapp-cache-again"),"CODE") should be(None)
+    di.firstMatchingData("aws-keys",App("frontend-article"),"CODE2") should be(None)
+    di.firstMatchingData("aws-keys",App("frontend-article"),"NEWCODE") should be(None)
+    di.firstMatchingData("aws-keys",App("new-microapp-cache"),"CODE") should be(None)
+    di.firstMatchingData("aws-keys",App("microapp-cache-again"),"CODE") should be(None)
   }
 
 }
