bff575fce1da335a50d89b7d08cba573e080e88d
Simon Hildrew <simon.hildrew@guardian.co.uk>
2012-10-30 16:59:29 +0000

    Add test to ensure that objects being serialised to mongoDB are not accidentally changed.

--- riff-raff/test/MongoDatastoreTest.scala
+++ riff-raff/test/MongoDatastoreTest.scala
@@ -11,6 +11,10 @@ import tasks.Task
 import deployment.Task
 import java.io.File
 import org.bson.BasicBSONEncoder
+import com.mongodb.util.JSON
+import com.mongodb.casbah.commons.ValidBSONType.DBObject
+import com.mongodb.DBObject
+import com.mongodb.casbah.commons.MongoDBObject
 
 case class UnserialisableTask(unserialisableFile:File) extends Task {
   def execute(sshCredentials: KeyRing) {}
@@ -70,6 +74,45 @@ class MongoDatastoreTest extends FlatSpec with ShouldMatchers {
     val encoder = new BasicBSONEncoder()
     val bytes = encoder.encode(dbObject)
     bytes should not be null
+  }
+
+  "Serialised case classes" should "not change" in {
+    // if this test fails then you have made a breaking change to the database model - don't just fix this!
+    val dataModelDump = """{ "_typeHint" : "deployment.DeployRecord" , "time" : { "$date" : "2012-11-08T17:20:00.000Z"} , "taskType" : "Deploy" , "_id" : { "$uuid" : "39320f5b-7837-4f47-85f7-bc2d780e19f6"} , "parameters" : { "_typeHint" : "magenta.DeployParameters" , "deployer" : { "_typeHint" : "magenta.Deployer" , "name" : "Tester"} , "build" : { "_typeHint" : "magenta.Build" , "projectName" : "test::project" , "id" : "1"} , "stage" : { "_typeHint" : "magenta.Stage" , "name" : "TEST"} , "recipe" : { "_typeHint" : "magenta.RecipeName" , "name" : "test-recipe"} , "hostList" : [ "testhost1" , "testhost2"]} , "messageStacks" : [ { "_typeHint" : "magenta.MessageStack" , "messages" : [ { "_typeHint" : "magenta.StartContext" , "originalMessage" : { "_typeHint" : "magenta.Deploy" , "parameters" : { "_typeHint" : "magenta.DeployParameters" , "deployer" : { "_typeHint" : "magenta.Deployer" , "name" : "Tester"} , "build" : { "_typeHint" : "magenta.Build" , "projectName" : "test::project" , "id" : "1"} , "stage" : { "_typeHint" : "magenta.Stage" , "name" : "TEST"} , "recipe" : { "_typeHint" : "magenta.RecipeName" , "name" : "test-recipe"} , "hostList" : [ "testhost1" , "testhost2"]}}}] , "time" : { "$date" : "2012-11-08T17:20:00.000Z"}} , { "_typeHint" : "magenta.MessageStack" , "messages" : [ { "_typeHint" : "magenta.Info" , "text" : "An information message"} , { "_typeHint" : "magenta.Verbose" , "text" : "A verbose message"} , { "_typeHint" : "magenta.CommandOutput" , "text" : "Some command stdout"} , { "_typeHint" : "magenta.CommandError" , "text" : "Some command stderr"} , { "_typeHint" : "magenta.Fail" , "text" : "A failure" , "detail" : { "_typeHint" : "magenta.ThrowableDetail" , "name" : "java.lang.RuntimeException" , "message" : "Test Exception" , "stackTrace" : "Long string\n With new lines\n and line numbers:5\n etc etc etc" , "cause" : { "_typeHint" : "magenta.ThrowableDetail" , "name" : "java.lang.RuntimeException" , "message" : "Test nested exception" , "stackTrace" : "Long string\n With new lines\n and line numbers:5\n etc etc etc"}}} , { "_typeHint" : "magenta.TaskRun" , "task" : { "_typeHint" : "magenta.TaskDetail" , "name" : "UnserialisableTask" , "description" : "A naughty and unserialisable task" , "verbose" : "Can't serialise me!" , "taskHosts" : [ { "_typeHint" : "magenta.Host" , "name" : "respub01" , "apps" : [ ] , "stage" : "NO_STAGE"}]}} , { "_typeHint" : "magenta.TaskList" , "taskList" : [ { "_typeHint" : "magenta.TaskDetail" , "name" : "UnserialisableTask" , "description" : "A naughty and unserialisable task" , "verbose" : "Can't serialise me!" , "taskHosts" : [ { "_typeHint" : "magenta.Host" , "name" : "respub01" , "apps" : [ ] , "stage" : "NO_STAGE"}]}]}] , "time" : { "$date" : "2012-11-08T17:20:00.000Z"}} , { "_typeHint" : "magenta.MessageStack" , "messages" : [ { "_typeHint" : "magenta.FailContext" , "originalMessage" : { "_typeHint" : "magenta.Deploy" , "parameters" : { "_typeHint" : "magenta.DeployParameters" , "deployer" : { "_typeHint" : "magenta.Deployer" , "name" : "Tester"} , "build" : { "_typeHint" : "magenta.Build" , "projectName" : "test::project" , "id" : "1"} , "stage" : { "_typeHint" : "magenta.Stage" , "name" : "TEST"} , "recipe" : { "_typeHint" : "magenta.RecipeName" , "name" : "test-recipe"} , "hostList" : [ "testhost1" , "testhost2"]}} , "detail" : { "_typeHint" : "magenta.ThrowableDetail" , "name" : "java.lang.RuntimeException" , "message" : "Test Exception" , "stackTrace" : "Long string\n With new lines\n and line numbers:5\n etc etc etc" , "cause" : { "_typeHint" : "magenta.ThrowableDetail" , "name" : "java.lang.RuntimeException" , "message" : "Test nested exception" , "stackTrace" : "Long string\n With new lines\n and line numbers:5\n etc etc etc"}}}] , "time" : { "$date" : "2012-11-08T17:20:00.000Z"}} , { "_typeHint" : "magenta.MessageStack" , "messages" : [ { "_typeHint" : "magenta.FinishContext" , "originalMessage" : { "_typeHint" : "magenta.Deploy" , "parameters" : { "_typeHint" : "magenta.DeployParameters" , "deployer" : { "_typeHint" : "magenta.Deployer" , "name" : "Tester"} , "build" : { "_typeHint" : "magenta.Build" , "projectName" : "test::project" , "id" : "1"} , "stage" : { "_typeHint" : "magenta.Stage" , "name" : "TEST"} , "recipe" : { "_typeHint" : "magenta.RecipeName" , "name" : "test-recipe"} , "hostList" : [ "testhost1" , "testhost2"]}}}] , "time" : { "$date" : "2012-11-08T17:20:00.000Z"}}]}"""
+
+    val time = new DateTime(2012,11,8,17,20,00)
+    val uuid = UUID.fromString("39320f5b-7837-4f47-85f7-bc2d780e19f6")
+    val parameters = DeployParameters(Deployer("Tester"), Build("test::project", "1"), Stage("TEST"), RecipeName("test-recipe"), List("testhost1", "testhost2"))
+    val testNestedDetail = ThrowableDetail("java.lang.RuntimeException", "Test nested exception", "Long string\n With new lines\n and line numbers:5\n etc etc etc")
+    val testThrowableDetail = ThrowableDetail("java.lang.RuntimeException", "Test Exception", "Long string\n With new lines\n and line numbers:5\n etc etc etc", Some(testNestedDetail))
+    val testTask = UnserialisableTask(new File("/tmp"))
+    val messageStacks = List(
+      MessageStack(List(StartContext(Deploy(parameters))), time),
+      MessageStack(List(
+        Info("An information message"),
+        Verbose("A verbose message"),
+        CommandOutput("Some command stdout"),
+        CommandError("Some command stderr"),
+        Fail("A failure", testThrowableDetail),
+        TaskRun(testTask),
+        TaskList(List(testTask))
+      ), time),
+      MessageStack(List(FailContext(Deploy(parameters), testThrowableDetail)), time),
+      MessageStack(List(FinishContext(Deploy(parameters))), time)
+    )
+    val deployRecord = DeployRecord(time, Task.Deploy, uuid, parameters, messageStacks)
+    val gratedDeployRecord = graters.recordGrater.asDBObject(deployRecord)
+
+    val jsonDeployRecord = JSON.serialize(gratedDeployRecord)
+    jsonDeployRecord should be(dataModelDump)
+
+    val ungratedDBObject = JSON.parse(dataModelDump).asInstanceOf[DBObject]
+    ungratedDBObject.toString should be(dataModelDump)
+
+    val mongoDbObject = new MongoDBObject(ungratedDBObject)
+
+    val ungratedDeployRecord = graters.recordGrater.asObject(mongoDbObject)
+    ungratedDeployRecord should be(deployRecord)
   }
 
 }
