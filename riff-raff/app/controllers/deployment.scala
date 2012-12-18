66fb3b7b3f5195ffc96d2c2671f84fd41dcbda97
Simon Hildrew <simon.hildrew@guardian.co.uk>
2012-12-18 10:43:16 +0000

    Fix copy paste error

--- riff-raff/app/controllers/deployment.scala
+++ riff-raff/app/controllers/deployment.scala
@@ -315,7 +315,7 @@ object Deployment extends Controller with Logging {
 
   def markAsFailed = AuthAction { implicit request =>
     uuidForm.bindFromRequest().fold(
-      errors => Redirect(routes.Deployment.viewUUID(form.uuid)),
+      errors => Redirect(routes.Deployment.history),
       form => {
         form.action match {
           case "markAsFailed" =>
