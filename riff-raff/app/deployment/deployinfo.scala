0c29fd55bdbfc19fcedb2f9a34ba78c6921c76df
Simon Hildrew <simon@hildrew.net>
2012-12-31 16:06:14 +0000

    Make deployinfo configurable

--- riff-raff/app/deployment/deployinfo.scala
+++ riff-raff/app/deployment/deployinfo.scala
@@ -6,32 +6,56 @@ import akka.actor.ActorSystem
 import akka.util.duration._
 import controllers.Logging
 import magenta.App
-import conf.Configuration
+import conf.{DeployInfoMode, Configuration}
 import utils.ScheduledAgent
-import java.io.File
+import java.io.{FileNotFoundException, File}
+import java.net.{URLConnection, Proxy, URL, URLStreamHandler}
+import play.api.libs.ws.WS
+import io.Source
 
 object DeployInfoManager extends Logging {
+  private val classpathHandler = new URLStreamHandler {
+    val classloader = getClass.getClassLoader
+    override def openConnection(u: URL): URLConnection = {
+      val resourceURL = classloader.getResource(u.getPath)
+      if (resourceURL == null)
+        throw new FileNotFoundException("%s not found on classpath" format u.getPath)
+      resourceURL.openConnection()
+    }
+  }
+
   private def getDeployInfo = {
     try {
       import sys.process._
       log.info("Populating deployinfo hosts...")
-      val deployInfo = if (new File(deployInfoLocation).exists)
-        DeployInfoJsonReader.parse(deployInfoLocation.!!)
-      else {
-        log.warn("No file found at '%s', defaulting to empty DeployInfo" format (deployInfoLocation))
-        DeployInfo(List(), Map())
+      val deployInfoJson: String = Configuration.deployinfo.mode match {
+        case DeployInfoMode.Execute =>
+          if (new File(Configuration.deployinfo.location).exists)
+            Configuration.deployinfo.location.!!
+          else {
+            log.warn("No file found at '%s', defaulting to empty DeployInfo" format (Configuration.deployinfo.location))
+            ""
+          }
+        case DeployInfoMode.URL =>
+          val url = Configuration.deployinfo.location match {
+            case classPathLocation if classPathLocation.startsWith("classpath:") => new URL(null, classPathLocation, classpathHandler)
+            case otherURL => new URL(otherURL)
+          }
+          log.info("URL: %s" format url)
+          Source.fromURL(url).getLines.mkString
       }
+
+      val deployInfo = DeployInfoJsonReader.parse(deployInfoJson)
 
       log.info("Successfully retrieved deployinfo (%d hosts and %d data found)" format (
         deployInfo.hosts.size, deployInfo.data.values.map(_.size).fold(0)(_+_)))
+
       deployInfo
     } catch {
       case e => log.error("Couldn't gather deployment information", e)
       throw e
     }
   }
-  // Should be configurable
-  val deployInfoLocation = "/opt/bin/deployinfo.json"
 
   val system = ActorSystem("deploy")
   val agent = ScheduledAgent[DeployInfo](1 minute, 1 minute)(getDeployInfo)
