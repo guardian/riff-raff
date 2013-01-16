a5e56525588170c018b9d0f6014cf5d55abc884b
Simon Hildrew <simon.hildrew@guardian.co.uk>
2013-01-16 13:50:36 +0000

    Further improvements to cancelling deploys
     - Moved preparation into the TaskRunner process
     - Can now cancel a deploy before it starts running

--- riff-raff/app/deployment/actors.scala
+++ riff-raff/app/deployment/actors.scala
@@ -14,15 +14,17 @@ import java.util.UUID
 import magenta.teamcity.Artifact.build2download
 import collection.mutable.ListBuffer
 import akka.routing.RoundRobinRouter
-import com.typesafe.config.ConfigFactory
 import akka.dispatch.Await
+import play.api.Play
+import play.api.Play.current
+import com.typesafe.config.ConfigFactory
 
 object DeployControlActor extends Logging {
   trait Event
   case class Deploy(record: Record) extends Event
 
-  lazy val config = ConfigFactory.load()
-  lazy val system = ActorSystem("deploy", config.getConfig("riffraff").withFallback(config))
+  lazy val config = Play.configuration.underlying
+  lazy val system = ActorSystem("deploy", config.getConfig("riffraff").withFallback(ConfigFactory.load()))
 
   lazy val deployController = system.actorOf(Props[DeployControlActor])
   lazy val deployCoordinator = system.actorOf(Props[DeployCoordinator])
@@ -34,20 +36,8 @@ object DeployControlActor extends Logging {
   import deployment.DeployCoordinator.{StopDeploy, StartDeploy}
 
   def interruptibleDeploy(record: Record) {
-    val loggingContext = MessageBroker.startDeployContext(record.uuid, record.parameters)
-    val deployMessage = MessageBroker.withContext(loggingContext) {
-      val artifactDir = record.parameters.build.download()
-      MessageBroker.info("Reading deploy.json")
-      val project = JsonReader.parse(new File(artifactDir, "deploy.json"))
-      val context = record.parameters.toDeployContext(record.uuid, project, DeployInfoManager.deployInfo)
-      if (context.tasks.isEmpty)
-        MessageBroker.fail("No tasks were found to execute. Ensure the app(s) are in the list supported by this stage/host.")
-      val keyRing = DeployInfoManager.keyRing(context)
-
-      StartDeploy(record, artifactDir, context, keyRing, loggingContext)
-    }
-    log.info("Sending start deploy mesage to co-ordinator")
-    deployCoordinator ! deployMessage
+    log.debug("Sending start deploy mesage to co-ordinator")
+    deployCoordinator ! StartDeploy(record)
   }
 
   def stopDeploy(uuid: UUID, userName: String) {
@@ -142,21 +132,19 @@ case class UniqueTask(id: Int, task: Task)
 
 case class DeployRunState(
   record: Record,
-  artifactDir: File,
-  context: DeployContext,
-  keyRing: KeyRing,
   loggingContext: MessageBrokerContext,
-  stopUserName: Option[String] = None,
-  stopFlag: Boolean = false
+  artifactDir: Option[File] = None,
+  context: Option[DeployContext] = None,
+  keyRing: Option[KeyRing] = None
 ) {
-  lazy val taskList = context.tasks.zipWithIndex.map(t => UniqueTask(t._2, t._1))
-  def firstTask = taskList.head
+  lazy val taskList = context.map(_.tasks.zipWithIndex.map(t => UniqueTask(t._2, t._1))).getOrElse(Nil)
+  def firstTask = taskList.headOption
   def nextTask(task: UniqueTask): Option[UniqueTask] = taskList.drop(task.id+1).headOption
 }
 
 object DeployCoordinator {
   trait Message
-  case class StartDeploy(record: Record, artifactDir: File, context: DeployContext, keyRing: KeyRing, loggingContext: MessageBrokerContext) extends Message
+  case class StartDeploy(record: Record) extends Message
   case class StopDeploy(uuid: UUID, userName: String) extends Message
   case class CheckStopFlag(uuid: UUID) extends Message
 }
@@ -170,10 +158,11 @@ class DeployCoordinator extends Actor with Logging {
   }
 
   val taskStrategy = OneForOneStrategy(maxNrOfRetries = 3, withinTimeRange = 1 minute) { case _ => Restart }
-  val runners = context.actorOf(Props[TaskRunner].withDispatcher("task-dispatcher").withRouter(new RoundRobinRouter(8).withSupervisorStrategy(taskStrategy)))
+  val runners = context.actorOf(Props[TaskRunner].withDispatcher("akka.task-dispatcher").withRouter(new RoundRobinRouter(8).withSupervisorStrategy(taskStrategy)))
 
   var deployStateMap = Map.empty[UUID, DeployRunState]
   var deferredDeployQueue = ListBuffer[DeployCoordinator.Message]()
+  var stopFlagMap = Map.empty[UUID, Option[String]]
 
   def schedulable(record: Record): Boolean = {
     deployStateMap.size < conf.Configuration.concurrency.maxDeploys &&
@@ -183,57 +172,81 @@ class DeployCoordinator extends Actor with Logging {
       ).isEmpty
   }
 
+  def ifStopFlagClear[T](state: DeployRunState)(block: => T): Option[T] = {
+    stopFlagMap.contains(state.record.uuid) match {
+      case true =>
+        log.debug("Stop flag set")
+        val stopMessage = "Deploy has been stopped by %s" format stopFlagMap(state.record.uuid).getOrElse("an unknown user")
+        MessageBroker.failAllContexts(state.loggingContext, stopMessage, new DeployStoppedException(stopMessage))
+        log.debug("Cleaning up")
+        cleanup(state)
+        None
+
+      case false =>
+        Some(block)
+    }
+  }
+
   protected def receive = {
-    case StartDeploy(record, artifactDir, deployContext, keyRing, loggingContext) if !schedulable(record) =>
-      log.info("Not schedulable, queuing")
-      // queue if already in progress
-      deferredDeployQueue += StartDeploy(record, artifactDir, deployContext, keyRing, loggingContext)
+    case StartDeploy(record) if !schedulable(record) =>
+      log.debug("Not schedulable, queuing")
+      deferredDeployQueue += StartDeploy(record)
 
-    case StartDeploy(record, artifactDir, deployContext, keyRing, loggingContext) if schedulable(record) =>
-      log.info("Starting first task")
-      val state = DeployRunState(record, artifactDir, deployContext, keyRing, loggingContext)
-      deployStateMap += (record.uuid -> state)
-      runners ! RunTask(state.record, state.keyRing, state.firstTask, loggingContext)
-
-    case StopDeploy(uuid, userName) =>
-      log.info("Processing deploy stop request")
-      deployStateMap.get(uuid).foreach { state =>
-        deployStateMap += (uuid -> state.copy(stopFlag = true, stopUserName = Some(userName)))
+    case StartDeploy(record) if schedulable(record) =>
+      log.debug("Scheduling deploy")
+      val loggingContext = MessageBroker.startDeployContext(record.uuid, record.parameters)
+      val state = DeployRunState(record, loggingContext)
+      ifStopFlagClear(state) {
+        deployStateMap += (record.uuid -> state)
+        runners ! PrepareDeploy(record, loggingContext)
       }
 
-    case TaskCompleted(record, task) =>
-      log.info("Task completed")
-      deployStateMap.get(record.uuid).foreach { state =>
-        state.stopFlag match {
-          case true =>
-            log.info("Stop flag set")
-            val stopMessage = "Deploy has been stopped by %s" format state.stopUserName.getOrElse("an unknown user")
-            MessageBroker.failAllContexts(state.loggingContext, stopMessage, new DeployStoppedException(stopMessage))
-            log.info("Cleaning up")
-            cleanup(state)
+    case StopDeploy(uuid, userName) =>
+      log.debug("Processing deploy stop request")
+      stopFlagMap += (uuid -> Some(userName))
 
-          case false =>
-            log.info("Stop flag clear")
-            // start next task
-            state.nextTask(task) match {
-              case Some(nextTask) =>
-                log.info("Running next task")
-                runners ! RunTask(state.record, state.keyRing, state.nextTask(task).get, state.loggingContext)
-              case None =>
-                MessageBroker.finishContext(state.loggingContext)
-                log.info("Cleaning up")
-                cleanup(state)
-            }
+    case DeployReady(record, artifactDir, deployContext, keyRing) =>
+      deployStateMap.get(record.uuid).foreach  { state =>
+        val newState = state.copy(
+          artifactDir = Some(artifactDir),
+          context = Some(deployContext),
+          keyRing = Some(keyRing)
+        )
+        deployStateMap += (record.uuid -> newState)
+        ifStopFlagClear(newState) {
+          newState.firstTask.foreach { task =>
+            log.debug("Starting first task")
+            runners ! RunTask(newState.record, newState.keyRing.get, task, newState.loggingContext)
+          }
+        }
+      }
+      
+    case TaskCompleted(record, task) =>
+      log.debug("Task completed")
+      deployStateMap.get(record.uuid).foreach { state =>
+        ifStopFlagClear(state) {
+          log.debug("Stop flag clear")
+          // start next task
+          state.nextTask(task) match {
+            case Some(nextTask) =>
+              log.debug("Running next task")
+              runners ! RunTask(state.record, state.keyRing.get, state.nextTask(task).get, state.loggingContext)
+            case None =>
+              MessageBroker.finishContext(state.loggingContext)
+              log.debug("Cleaning up")
+              cleanup(state)
+          }
         }
       }
 
     case TaskFailed(record, exception) =>
-      log.info("Task failed")
+      log.debug("Task failed")
       deployStateMap.get(record.uuid).foreach(cleanup)
 
     case CheckStopFlag(uuid) =>
       try {
-        val stopFlag = deployStateMap.get(uuid).map(_.stopFlag).getOrElse(false)
+        val stopFlag = stopFlagMap.contains(uuid)
+        log.debug("stop flag requested for %s, responding with %b" format (uuid, stopFlag))
         sender ! stopFlag
       } catch {
         case e:Exception =>
@@ -245,12 +258,18 @@ class DeployCoordinator extends Actor with Logging {
   }
 
   private def cleanup(state: DeployRunState) {
-    sbt.IO.delete(state.artifactDir)
+    try {
+      sbt.IO.delete(state.artifactDir)
+    } catch {
+      case t:Throwable =>
+        log.warn("Exception whilst trying to delete artifact directory", t)
+    }
 
     deferredDeployQueue.foreach(self ! _)
     deferredDeployQueue.clear()
 
     deployStateMap -= state.record.uuid
+    stopFlagMap -= state.record.uuid
   }
 
 }
@@ -261,6 +280,8 @@ object TaskRunner {
   case class TaskCompleted(record: Record, task: UniqueTask) extends Message
   case class TaskFailed(record: Record, exception: Throwable) extends Message
 
+  case class PrepareDeploy(record: Record, loggingContext: MessageBrokerContext) extends Message
+  case class DeployReady(record: Record, artifactDir: File, context: DeployContext, keyRing: KeyRing) extends Message
   case class RemoveArtifact(artifactDir: File) extends Message
 }
 
@@ -268,8 +289,28 @@ class TaskRunner extends Actor with Logging {
   import TaskRunner._
 
   protected def receive = {
+    case PrepareDeploy(record, loggingContext) =>
+      try {
+        MessageBroker.withContext(loggingContext) {
+          val artifactDir = record.parameters.build.download()
+          MessageBroker.info("Reading deploy.json")
+          val project = JsonReader.parse(new File(artifactDir, "deploy.json"))
+          val context = record.parameters.toDeployContext(record.uuid, project, DeployInfoManager.deployInfo)
+          if (context.tasks.isEmpty)
+            MessageBroker.fail("No tasks were found to execute. Ensure the app(s) are in the list supported by this stage/host.")
+          val keyRing = DeployInfoManager.keyRing(context)
+
+          sender ! DeployReady(record, artifactDir, context, keyRing)
+        }
+      } catch {
+        case t:Throwable =>
+          log.debug("Preparing deploy failed")
+          sender ! TaskFailed(record, t)
+      }
+
+
     case RunTask(record, keyring, task, loggingContext) => {
-      log.info("Running task %d" format task.id)
+      log.debug("Running task %d" format task.id)
       try {
         def stopFlagAsker: Boolean = {
           implicit val timeout = Timeout(200 milliseconds)
@@ -281,17 +322,17 @@ class TaskRunner extends Actor with Logging {
             task.task.execute(keyring, stopFlagAsker)
           }
         }
-        log.info("Sending completed message")
+        log.debug("Sending completed message")
         sender ! TaskCompleted(record, task)
       } catch {
         case t:Throwable =>
-          log.info("Sending failed message")
+          log.debug("Sending failed message")
           sender ! TaskFailed(record, t)
       }
     }
 
     case RemoveArtifact(artifactDir) => {
-      log.info("Delete artifact dir")
+      log.debug("Delete artifact dir")
       sbt.IO.delete(artifactDir)
     }
   }
