6c0d489ebf68a42a9bd6d13aa893a5973eb64e2c
Simon Hildrew <simon.hildrew@guardian.co.uk>
2013-05-29 16:52:25 +0100

    Instantiate a new parser for every request as it is not threadsafe

--- riff-raff/app/docs/markdown.scala
+++ riff-raff/app/docs/markdown.scala
@@ -4,8 +4,7 @@ import org.clapper.markwrap.{MarkupType, MarkWrap}
 import play.api.templates.Html
 
 object MarkDown {
-  val parser = MarkWrap.parserFor(MarkupType.Markdown)
-  def toHtml(markDown: String): Html = {
-    Html(parser.parseToHTML(markDown))
+    def toHtml(markDown: String): Html = {
+    Html(MarkWrap.parserFor(MarkupType.Markdown).parseToHTML(markDown))
   }
 }
