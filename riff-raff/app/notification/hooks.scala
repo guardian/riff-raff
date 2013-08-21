ded55972d7ac01223f28fade2be9525c09f849f5
Simon Hildrew <simon.hildrew@guardian.co.uk>
2013-08-21 18:30:27 +0100

    Add and fix tests

--- riff-raff/app/notification/hooks.scala
+++ riff-raff/app/notification/hooks.scala
@@ -6,7 +6,7 @@ import akka.actor.{Actor, Props, ActorSystem}
 import lifecycle.LifecycleWithoutApp
 import persistence.{MongoFormat, MongoSerialisable, Persistence}
 import deployment.TaskType
-import java.net.URL
+import java.net.{URLEncoder, URL}
 import com.mongodb.casbah.commons.{Imports, MongoDBObject}
 import magenta.FinishContext
 import magenta.DeployParameters
@@ -34,8 +34,8 @@ case class Auth(user:String, password:String, scheme:AuthScheme=AuthScheme.BASIC
 case class HookAction(url: String, enabled: Boolean) extends Logging with MongoSerialisable[HookAction] {
   val substitutionPoint = """%deploy\.([A-Za-z]+)%""".r
   def request(uuid: UUID, parameters: DeployParameters) = {
-    substitutionPoint.replaceAllIn(url, (substitution) => {
-      substitution.group(1).toLowerCase match {
+    val newUrl = substitutionPoint.replaceAllIn(url, (substitution) => {
+      URLEncoder.encode(substitution.group(1).toLowerCase match {
         case "build" => parameters.build.id
         case "project" => parameters.build.projectName
         case "stage" => parameters.stage.name
@@ -43,22 +43,23 @@ case class HookAction(url: String, enabled: Boolean) extends Logging with MongoS
         case "hosts" => parameters.hostList.mkString(",")
         case "deployer" => parameters.deployer.name
         case "uuid" => uuid.toString
-      }
+      }, "utf-8")
     })
-    val userInfo = Option(new URL(url).getUserInfo).flatMap { ui =>
+    val userInfo = Option(new URL(newUrl).getUserInfo).flatMap { ui =>
       val elements = ui.split(':')
       if (elements.length == 2)
         Some(Auth(elements(0), elements(1)))
       else
         None
     }
-    userInfo.map(ui => WS.url(url).withAuth(ui.user, ui.password, ui.scheme)).getOrElse(WS.url(url))
+    userInfo.map(ui => WS.url(newUrl).withAuth(ui.user, ui.password, ui.scheme)).getOrElse(WS.url(newUrl))
   }
 
   def act(uuid: UUID, parameters: DeployParameters) {
     if (enabled) {
-      log.info("Calling %s" format url)
-      request(uuid, parameters).get().map { response =>
+      val urlRequest = request(uuid, parameters)
+      log.info("Calling %s" format urlRequest.url)
+      urlRequest.get().map { response =>
         log.info("HTTP status code %d" format response.status)
         log.debug("HTTP response body %s" format response.body)
       }
