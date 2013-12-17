a988980ece7b5dd4c1eb9180b0d6d586feec7937
Simon Hildrew <simon.hildrew@guardian.co.uk>
2013-12-17 17:29:16 +0000

    Ensure that bad or incomplete instance data from Prism is handled gracefully

--- riff-raff/app/resources/PrismLookup.scala
+++ riff-raff/app/resources/PrismLookup.scala
@@ -12,8 +12,9 @@ import utils.Json._
 import org.joda.time.format.DateTimeFormat
 import scala.util.control.NonFatal
 import controllers.Logging
+import scala.util.{Failure, Success, Try}
 
-object PrismLookup extends Lookup with MagentaCredentials {
+object PrismLookup extends Lookup with MagentaCredentials with Logging {
   object prism extends Logging {
     val url = conf.Configuration.lookup.prismUrl
     def get[T](path: String, retriesLeft: Int = 5)(block: JsValue => T): T = {
@@ -79,15 +80,31 @@ object PrismLookup extends Lookup with MagentaCredentials {
   }
 
   def instances = new Instances {
-    def parseHosts(json: JsValue):Seq[Host] =
-      (json \ "data" \ "instances").as[Seq[Host]].flatMap{ host =>
-        host.apps match {
-          case singleApp if singleApp.size == 1 => Seq(host)
-          case noApps if noApps.isEmpty => None
-          case multipleApps =>
-            multipleApps.toSeq.map( app => host.copy(apps = Set(app)))
+    def parseHosts(json: JsValue):Seq[Host] = {
+      val tryHosts = (json \ "data" \ "instances").as[JsArray].value.map { jsHost =>
+        Try {
+          val host = jsHost.as[Host]
+          host.apps match {
+            case singleApp if singleApp.size == 1 => Seq(host)
+            case noApps if noApps.isEmpty => Nil
+            case multipleApps =>
+              multipleApps.toSeq.map( app => host.copy(apps = Set(app)))
+          }
         }
       }
+
+      val errors = tryHosts.flatMap {
+        case f@Failure(e) => Some(f)
+        case _ => None
+      }
+      if (errors.size > 0) log.warn(s"Encountered ${errors.size} (of ${tryHosts.size}) instance records that could not be parsed in Prism response")
+      if (log.isDebugEnabled) errors.foreach(e => log.debug("Couldn't parse instance from Prism data", e.exception))
+
+      tryHosts.flatMap {
+        case Success(hosts) => hosts
+        case _ => Nil
+      }
+    }
 
     def get(app: App, stage: Stage): Seq[Host] =
       prism.get(s"/instances?_expand&stage=${stage.name}&mainclasses=${app.name}")(parseHosts)
