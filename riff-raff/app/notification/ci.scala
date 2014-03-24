49d0e6de548cd4db7fdf98ad888366948739fcb9
Philip Wills <philip.wills@guardian.co.uk>
2014-03-24 17:49:36 +0000

    Backing out changes which should have been in another branch

--- riff-raff/app/notification/ci.scala
+++ riff-raff/app/notification/ci.scala
@@ -11,12 +11,11 @@ import magenta.Deploy
 import scala.Some
 import conf.Configuration
 import java.util.UUID
-import ci.teamcity.{TeamCity, BuildType}
+import ci.teamcity.BuildType
 import ci.teamcity.TeamCity.BuildLocator
 import play.api.libs.concurrent.Promise
 import play.api.libs.concurrent.Execution.Implicits._
 import scala.concurrent.Future
-import play.api.libs.ws.Response
 
 object TeamCityBuildPinner extends LifecycleWithoutApp with Logging {
 
@@ -41,18 +40,10 @@ object TeamCityBuildPinner extends LifecycleWithoutApp with Logging {
     log.info("Pinning build %s" format build.toString)
     val tcBuild = TeamCityBuilds.build(build.projectName,build.id)
     tcBuild.map { realBuild =>
-      def pin(text: String): Future[Response] = {
-        val buildPinCall = TeamCity.api.build.pin(BuildLocator.id(realBuild.id)).put(text)
-        buildPinCall.map { response =>
-          log.info(s"Pinning build $realBuild: HTTP status code ${response.status}")
-          log.debug(s"HTTP response body ${response.body}")
-        }
-        buildPinCall
-      }
-      pin(s"Pinned by RiffRaff: ${Configuration.urls.publicPrefix}${routes.Deployment.viewUUID(deployId.toString).url}")
+      realBuild.pin("Pinned by RiffRaff: %s%s" format (Configuration.urls.publicPrefix, routes.Deployment.viewUUID(deployId.toString).url))
       cleanUpPins(realBuild.buildType)
     } getOrElse {
-      log.warn(s"Unable to pin build ${build} as the associated TeamCity build was not known")
+      log.warn("Unable to pin build %s as the associated TeamCity build was not known" format build.toString)
     }
   }
 
@@ -67,15 +58,7 @@ object TeamCityBuildPinner extends LifecycleWithoutApp with Logging {
           log.debug("Got details for %d builds: %s" format (detailedBuilds.size, detailedBuilds.mkString("\n")))
           detailedBuilds.filter(_.pinInfo.get.user.username == tcUserName).sortBy(-_.pinInfo.get.timestamp.getMillis).drop(maxPinned).map { buildToUnpin =>
             log.debug("Unpinning %s" format buildToUnpin)
-            def unpin(): Future[Response] = {
-              val buildPinCall = TeamCity.api.build.pin(BuildLocator.id(buildToUnpin.id)).delete()
-              buildPinCall.map { response =>
-                log.info(s"Unpinning build $buildToUnpin: HTTP status code ${response.status}")
-                log.debug(s"HTTP response body ${response.body}")
-              }
-              buildPinCall
-            }
-            unpin()
+            buildToUnpin.unpin()
           }
         }
       }
