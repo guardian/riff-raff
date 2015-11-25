7be59fd0597bf05535e18a93fd696f06f88eecb6
Gideon Goldberg <gidsgoldberg@gmail.com>
2015-11-25 12:20:35 +0000

    get function name from map

--- magenta-lib/src/main/scala/magenta/deployment_type/Lambda.scala
+++ magenta-lib/src/main/scala/magenta/deployment_type/Lambda.scala
@@ -7,20 +7,30 @@ object Lambda extends DeploymentType  {
   val name = "aws-lambda"
   val documentation =
     """
-      |Provides one deploy action, `updateLambda`, that runs Lambda Update Function Code using the package file which should be a single .zip file.
+      |Provides one deploy action, `updateLambda`, that runs Lambda Update Function Code using the package file which should be a single file named lambda.zip.
     """.stripMargin
   
 
   //required configuration, you cannot upload without setting these
-  val functionName = Param[String]("functionName", "Lambda function name")
+  val functionNames = Param[Map[String, String]]("functionNames",
+    documentation =
+      """Map of Stage to Lambda function names.
+        |e.g.
+        |        "functionNames": {
+        |          "CODE": 'myLambda-CODE',
+        |          "PROD": 'myLambda-PROD'
+        |        }
+      """.stripMargin
+  ).default(Map.empty)
 
   def perAppActions = {
     case "updateLambda" => (pkg) => (resourceLookup, parameters, stack) => {
       implicit val keyRing = resourceLookup.keyRing(parameters.stage, pkg.apps.toSet, stack)
-      val fName = functionName(pkg)
+      val fNames = functionNames(pkg)
       val stage = parameters.stage.name
-      assert(functionName.get(pkg).isDefined, "functionName must be defined")
-    List(UpdateLambda(new File(pkg.srcDir.getPath + "/lambda.zip"), s"$fName-$stage"))
+      val fName = fNames get stage
+      assert(fName.isDefined, s"functionName must be defined for stage $stage")
+    List(UpdateLambda(new File(pkg.srcDir.getPath + "/lambda.zip"), fName.get))
 
     }
   }
