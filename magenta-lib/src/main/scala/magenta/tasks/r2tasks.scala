d7d52af00b8b9f8d6f728080f4f5bc2874485ccb
Philip Wills <philip.wills@guardian.co.uk>
2016-04-22 17:12:37 +0100

    Start cleaning up dead code now R2 is gone

--- magenta-lib/src/main/scala/magenta/tasks/r2tasks.scala
+++
@@ -1,8 +0,0 @@
-package magenta.tasks
-
-import magenta.{KeyRing, Host}
-
-case class ExtractToDocroots(host: Host, zipLocation: String, docrootType: String, locationInDocroot: String)
-                            (implicit val keyRing: KeyRing) extends RemoteShellTask {
-  def commandLine = List("/opt/bin/deploy_static_files.sh", zipLocation, docrootType, locationInDocroot)
-}
