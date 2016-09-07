e78651a11656f5625775ac8c8b42815dce2a3cad
Chris Birchall <chris.birchall@guardian.co.uk>
2016-09-07 17:35:32 +0100

    Enable filters

--- riff-raff/app/AppComponents.scala
+++ riff-raff/app/AppComponents.scala
@@ -1,15 +1,25 @@
+import conf.PlayRequestMetrics
 import controllers._
 import play.api.ApplicationLoader.Context
 import play.api.BuiltInComponentsFromContext
 import play.api.i18n.I18nComponents
 import play.api.libs.ws.ahc.AhcWSComponents
 import play.api.routing.Router
+import play.filters.gzip.GzipFilter
+import utils.HstsFilter
+
 import router.Routes
 
 class AppComponents(context: Context) extends BuiltInComponentsFromContext(context) with AhcWSComponents with I18nComponents {
 
   implicit val implicitMessagesApi = messagesApi
   implicit val implicitWsClient = wsClient
+
+  override lazy val httpFilters = Seq(
+    // TODO CSRF filter
+    new GzipFilter,
+    new HstsFilter
+  ) // TODO this would require an upgrade of the management-play lib ++ PlayRequestMetrics.asFilters
 
   val applicationController = new Application()(environment, wsClient)
   val deployController = new DeployController
@@ -19,6 +29,10 @@ class AppComponents(context: Context) extends BuiltInComponentsFromContext(conte
   val loginController = new Login
   val testingController = new Testing
   val assets = new Assets(httpErrorHandler)
+
+  // TODO custom error page (previously done in Global.onError)
+
+  // TODO set up lifecycles
 
   override def router: Router = new Routes(
     httpErrorHandler,
