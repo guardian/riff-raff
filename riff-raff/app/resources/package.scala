7031b3ef3de1651fefe7f5d5498cf0c654311f7f
Chris Birchall <chris.birchall@guardian.co.uk>
2016-09-08 19:07:57 +0100

    Yet more traces of deployinfo

--- riff-raff/app/resources/package.scala
+++ riff-raff/app/resources/package.scala
@@ -6,12 +6,9 @@ import magenta.Lookup
 import java.net.URLEncoder
 
 object `package` {
-  implicit class LookupWithStale(lookup: Lookup) {
-    def stale: Boolean = {
-      new Duration(lookup.lastUpdated, new DateTime).getStandardMinutes > Configuration.lookup.staleMinutes
-    }
-  }
+
   implicit class String2UrlEncode(string: String) {
     def urlEncode: String = URLEncoder.encode(string, "UTF-8")
   }
+
 }
