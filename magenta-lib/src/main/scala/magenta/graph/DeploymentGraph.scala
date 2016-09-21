c6393cbc76f43438a1d90fb7fad3407159a7dc58
Simon Hildrew <simon.hildrew@theguardian.com>
2016-09-21 11:06:02 +0100

    Rename Tasks to DeploymentTasks

--- magenta-lib/src/main/scala/magenta/graph/DeploymentGraph.scala
+++ magenta-lib/src/main/scala/magenta/graph/DeploymentGraph.scala
@@ -2,13 +2,13 @@ package magenta.graph
 
 import magenta.tasks.Task
 
-case class Tasks(tasks: List[Task], name: String)
+case class DeploymentTasks(tasks: List[Task], name: String)
 
 object DeploymentGraph {
-  def apply(tasks: List[Task], name: String, priority: Int = 1): Graph[Tasks] = {
-    val deploymentNode = MidNode(Tasks(tasks, name))
+  def apply(tasks: List[Task], name: String, priority: Int = 1): Graph[DeploymentTasks] = {
+    val deploymentNode = MidNode(DeploymentTasks(tasks, name))
     Graph(StartNode ~> deploymentNode, deploymentNode ~> EndNode)
   }
 
-  def toTaskList(taskGraph: Graph[Tasks]) = taskGraph.toList.flatMap(_.tasks)
+  def toTaskList(taskGraph: Graph[DeploymentTasks]) = taskGraph.toList.flatMap(_.tasks)
 }
