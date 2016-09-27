9ae053e997ac7f05ee4a023c1abc9612ca5a7023
Simon Hildrew <simon.hildrew@theguardian.com>
2016-09-27 18:52:31 +0100

    Add a new test with many layers of dependencies

--- magenta-lib/src/test/scala/magenta/input/resolver/DeploymentTasksGraphBuilderTest.scala
+++ magenta-lib/src/test/scala/magenta/input/resolver/DeploymentTasksGraphBuilderTest.scala
@@ -59,4 +59,31 @@ class DeploymentTasksGraphBuilderTest extends FlatSpec with ShouldMatchers {
       MidNode(deployment) ~2~> MidNode(deployment3), MidNode(deployment3) ~> EndNode
     )
   }
+
+  it should "build a graph with a long chain of dependencies" in {
+    val bob2a = deployment.copy(name = "bob2a", dependencies = List("bob"))
+    val bob2b = deployment.copy(name = "bob2b", dependencies = List("bob"))
+    val bob3 = deployment.copy(name = "bob3", dependencies = List("bob2a", "bob2b"))
+    val bob4 = deployment.copy(name = "bob4", dependencies = List("bob3"))
+    val bob5a = deployment.copy(name = "bob5a", dependencies = List("bob4"))
+    val bob5b = deployment.copy(name = "bob5b", dependencies = List("bob4"))
+    val deployments = List(deployment, bob2a, bob2b, bob3, bob4, bob5a, bob5b)
+    val graph = DeploymentGraphBuilder.buildGraph(deployments)
+
+    val bobNode = MidNode(deployment)
+    val bob2aNode = MidNode(bob2a)
+    val bob2bNode = MidNode(bob2b)
+    val bob3Node = MidNode(bob3)
+    val bob4Node = MidNode(bob4)
+    val bob5aNode = MidNode(bob5a)
+    val bob5bNode = MidNode(bob5b)
+    graph shouldBe Graph(
+      StartNode ~> bobNode,
+      bobNode ~> bob2aNode, bobNode ~2~> bob2bNode,
+      bob2aNode ~> bob3Node, bob2bNode ~> bob3Node,
+      bob3Node ~> bob4Node,
+      bob4Node ~> bob5aNode, bob4Node ~2~> bob5bNode,
+      bob5aNode ~> EndNode, bob5bNode ~> EndNode
+    )
+  }
 }
