df10838bc5f9eca7480c62540869c5e1e02fbcb0
Simon Hildrew <simon.hildrew@theguardian.com>
2016-09-28 15:54:17 +0100

    Inline method call and rename intermediate variables

--- magenta-lib/src/main/scala/magenta/input/resolver/DeploymentGraphBuilder.scala
+++ magenta-lib/src/main/scala/magenta/input/resolver/DeploymentGraphBuilder.scala
@@ -5,23 +5,19 @@ import magenta.input.Deployment
 
 object DeploymentGraphBuilder {
   def buildGraph(deployments: List[Deployment]): Graph[Deployment] = {
-    val edges = allEdges(deployments)
-    Graph(edges.toSet)
-  }
-
-  private[resolver] def allEdges(deployments: List[Deployment]) = {
     val deploymentNodes = deployments.map(MidNode.apply)
-    for {
-      from <- StartNode :: deploymentNodes
+    val edges = for {
+      dependency <- StartNode :: deploymentNodes
       edge <- {
-        val targets = from match {
+        val dependents = dependency match {
           case StartNode => deploymentNodes.filterNot(_.value.dependencies.exists(deployments.map(_.name).contains))
           case MidNode(deployment) => deploymentNodes.filter(_.value.dependencies.contains(deployment.name))
           case EndNode => throw new IllegalStateException("EndNode is not valid as the source of an edge")
         }
-        val to = if (targets.nonEmpty) targets else List(EndNode)
-        to.map(from ~>).reprioritise
+        val toNodes = if (dependents.nonEmpty) dependents else List(EndNode)
+        toNodes.map(dependency ~>).reprioritise
       }
     } yield edge
+    Graph(edges.toSet)
   }
 }
