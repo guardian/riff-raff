ddada49bb60d6548000f5e0b7e0ac2caaf5150b2
Philip Wills <philip.wills@guardian.co.uk>
2016-11-24 14:23:26 +0000

    Support Github HTTPS URLs

---
+++ riff-raff/test/utils/VCSInfoTest.scala
@@ -0,0 +1,18 @@
+package utils
+
+import java.net.URI
+
+import org.scalatest.{FunSuite, ShouldMatchers}
+
+
+class VCSInfoTest extends FunSuite with ShouldMatchers {
+  test("extracts details from Github HTTPS URL") {
+    val info = VCSInfo("https://github.com/guardian/contributions-frontend", "98a1cffadbe564d570b15c2113c07a28cbe835ee")
+    info.get.baseUrl shouldBe new URI("https://github.com/guardian/contributions-frontend")
+  }
+
+  test("extracts details from Github git URL") {
+    val info = VCSInfo("git@github.com:guardian/contributions-frontend.git", "98a1cffadbe564d570b15c2113c07a28cbe835ee")
+    info.get.baseUrl shouldBe new URI("https://github.com/guardian/contributions-frontend")
+  }
+}
