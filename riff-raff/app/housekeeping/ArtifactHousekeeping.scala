f6ea6a3ae8da92929f8d6c176760790efd5dda67
Simon Hildrew <simon@hildrew.net>
2018-07-26 11:01:39 +0100

    Move constants to config and add enabled flag

--- riff-raff/app/housekeeping/ArtifactHousekeeping.scala
+++ riff-raff/app/housekeeping/ArtifactHousekeeping.scala
@@ -10,12 +10,7 @@ import org.joda.time.{DateTime, Duration}
 import scala.annotation.tailrec
 import scala.collection.JavaConverters._
 
-class ArtifactHousekeeping(deployments: Deployments) extends Loggable {
-  private val s3Client = Configuration.artifact.aws.client
-  private val artifactBucketName = Configuration.artifact.aws.bucketName
-  private val NUMBER_OF_ARTIFACTS_TO_KEEP = 5
-  private val MINIMUM_ARTIFACT_AGE_DAYS = 40
-
+object ArtifactHousekeeping {
   @tailrec
   private def pagedAwsRequest[T](continuationToken: Option[String] = None, acc: List[T] = Nil)(f: Option[String] => (List[T], Option[String])): List[T] = {
     val (values: List[T], nextToken: Option[String]) = f(continuationToken)
@@ -30,7 +25,7 @@ class ArtifactHousekeeping(deployments: Deployments) extends Loggable {
     pagedAwsRequest(){ token =>
       val request = new ListObjectsV2Request()
         .withDelimiter("/")
-        .withBucketName(artifactBucketName)
+        .withBucketName(bucket)
         .withContinuationToken(token.orNull)
       val result = client.listObjectsV2(request)
       result.getCommonPrefixes.asScala.toList.map(_.stripSuffix("/")) -> Option(result.getNextContinuationToken)
@@ -42,29 +37,35 @@ class ArtifactHousekeeping(deployments: Deployments) extends Loggable {
     pagedAwsRequest(){ token =>
       val request = new ListObjectsV2Request()
         .withDelimiter("/")
-        .withBucketName(artifactBucketName)
+        .withBucketName(bucket)
         .withPrefix(prefix)
         .withContinuationToken(token.orNull)
       val result = client.listObjectsV2(request)
       result.getCommonPrefixes.asScala.toList.map(_.stripPrefix(prefix).stripSuffix("/")) -> Option(result.getNextContinuationToken)
     }
   }
+}
+
+class ArtifactHousekeeping(deployments: Deployments) extends Loggable {
+  private val s3Client = Configuration.artifact.aws.client
+  private val artifactBucketName = Configuration.artifact.aws.bucketName
 
   def getBuildIdsToKeep(projectName: String) = {
     val deployList = deployments.getDeploys(
       filter = Some(DeployFilter(projectName = Some(s"^$projectName$$"))),
-      pagination = PaginationView(pageSize=Some(50))
+      pagination = PaginationView(pageSize=Some(Configuration.housekeeping.tagOldArtifacts.numberToScan))
     )
     val perStageDeploys = deployList.groupBy(_.stage).values
-    val deploysToKeep = perStageDeploys.flatMap(_.sortBy(-_.time.getMillis).take(NUMBER_OF_ARTIFACTS_TO_KEEP))
+    val deploysToKeep = perStageDeploys.flatMap(_.sortBy(-_.time.getMillis).take(Configuration.housekeeping.tagOldArtifacts.numberToKeep))
     deploysToKeep.map(_.buildId)
   }
 
   def cleanUpTheBuilds(client: AmazonS3, bucket: String, projectName: String, buildsToDelete: Set[String], now: DateTime): Unit = {
-    val tag = new ObjectTagging(List(new Tag("housekeeping", "delete")).asJava)
+    val tag = new Tag(Configuration.housekeeping.tagOldArtifacts.tagKey, Configuration.housekeeping.tagOldArtifacts.tagValue)
+    val taggingObj = new ObjectTagging(List(tag).asJava)
     buildsToDelete.foreach { buildId =>
       logger.info(s"Tagging build ID $buildId")
-      val objects = pagedAwsRequest() { token =>
+      val objects = ArtifactHousekeeping.pagedAwsRequest() { token =>
         val request = new ListObjectsV2Request()
           .withDelimiter("/")
           .withBucketName(artifactBucketName)
@@ -76,12 +77,12 @@ class ArtifactHousekeeping(deployments: Deployments) extends Loggable {
 
       val objectsToTag = objects.filter { obj =>
         val age = new Duration(new DateTime(obj.getLastModified), now)
-        age.getStandardDays > MINIMUM_ARTIFACT_AGE_DAYS
+        age.getStandardDays > Configuration.housekeeping.tagOldArtifacts.minimumAgeDays
       }
 
       objectsToTag.foreach { obj =>
         logger.info(s"Tagging ${obj.getKey}")
-        val request = new SetObjectTaggingRequest(bucket, obj.getKey, tag)
+        val request = new SetObjectTaggingRequest(bucket, obj.getKey, taggingObj)
         client.setObjectTagging(request)
       }
       Thread.sleep(500)
@@ -89,19 +90,23 @@ class ArtifactHousekeeping(deployments: Deployments) extends Loggable {
   }
 
   def housekeepArtifacts(now: DateTime) = {
-    logger.info("Running housekeeping")
-    val projectNames = getProjectNames(s3Client, artifactBucketName)
-    projectNames.foreach { name =>
-      logger.info(s"Housekeeping project '$name'")
-      val buildIdsForProject = getBuildIds(s3Client, artifactBucketName, name).toSet
-      val buildIdsToKeep = getBuildIdsToKeep(name).toSet
-      val missingBuilds = buildIdsToKeep -- buildIdsForProject
-      if (missingBuilds.nonEmpty) {
-        logger.error("Some builds we wanted to keep were not found, possible something is awry.")
-      } else {
-        val buildsToDelete = buildIdsForProject -- buildIdsToKeep
-        cleanUpTheBuilds(s3Client, artifactBucketName, name, buildsToDelete, now)
+    if (Configuration.housekeeping.tagOldArtifacts.enabled) {
+      logger.info("Running housekeeping")
+      val projectNames = ArtifactHousekeeping.getProjectNames(s3Client, artifactBucketName)
+      projectNames.foreach { name =>
+        logger.info(s"Housekeeping project '$name'")
+        val buildIdsForProject = ArtifactHousekeeping.getBuildIds(s3Client, artifactBucketName, name).toSet
+        val buildIdsToKeep = getBuildIdsToKeep(name).toSet
+        val missingBuilds = buildIdsToKeep -- buildIdsForProject
+        if (missingBuilds.nonEmpty) {
+          logger.error("Some builds we wanted to keep were not found, possible something is awry.")
+        } else {
+          val buildsToDelete = buildIdsForProject -- buildIdsToKeep
+          cleanUpTheBuilds(s3Client, artifactBucketName, name, buildsToDelete, now)
+        }
       }
+    } else {
+      logger.info("Artifact housekeeping not enabled - skipping")
     }
   }
 }
