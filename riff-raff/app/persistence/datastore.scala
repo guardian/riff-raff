a1256fe081b9f3d3641f6915f1cb0fa0d5547661
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-10-10 16:23:21 +0100

    Delete mongo code for: keyValues

--- riff-raff/app/persistence/datastore.scala
+++ riff-raff/app/persistence/datastore.scala
@@ -40,9 +40,6 @@ trait DataStore extends DocumentStore {
   def getApiKeyByApplication(application: String): Option[ApiKey] = None
   def deleteApiKey(key: String) {}
 
-  def writeKey(key:String, value:String) {}
-  def readKey(key:String): Option[String] = None
-  def deleteKey(key: String) {}
 }
 
 object Persistence extends Logging {
