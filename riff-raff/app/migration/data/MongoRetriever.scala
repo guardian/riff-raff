03530301bdaadbb54baa4634258cd204182ea91b
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2019-01-30 10:14:35 +0000

    Pagination

--- riff-raff/app/migration/data/MongoRetriever.scala
+++ riff-raff/app/migration/data/MongoRetriever.scala
@@ -21,10 +21,15 @@ trait MongoRetriever[A] {
           // `size` may be larger than `max`, 
           .map(_.take(max))
           .flatMap { items =>
-            queue.offerAll(items) *> loop(page + 1, max - items.size)
+            // we may also get less elements than required
+            // which indicates the end of the table
+            if (items.size < max)
+              queue.offerAll(items)
+            else
+              queue.offerAll(items) *> loop(page + 1, max - items.size)
           }
 
-    loop(0, max)
+    loop(1, max)
   }
 }
 
@@ -33,28 +38,28 @@ object MongoRetriever {
     val F = F0
     val getCount = IO.sync(Persistence.store.collectionStats.get("deployV2").map(_.documentCount.toInt).getOrElse(0))
     def getItems(pagination: PaginationView) =
-      IO.flatten(IO.sync(IO.fromEither(Persistence.store.getDeploys(None, pagination)))).leftMap(DatabaseError)
+      IO.flatten(IO.sync(IO.fromEither { Persistence.store.getDeploys(None, pagination).map(_.toList) })).leftMap(DatabaseError)
     }
     
   def logRetriever(implicit F0: MongoFormat[LogDocument]) = new MongoRetriever[LogDocument] {
     val F = F0
     val getCount = IO.sync(Persistence.store.collectionStats.get("deployV2Logs").map(_.documentCount.toInt).getOrElse(0))
     def getItems(pagination: PaginationView) =
-      IO.flatten(IO.sync(IO.fromEither(Persistence.store.readAllLogs(pagination)))).leftMap(DatabaseError)
+      IO.flatten(IO.sync(IO.fromEither { Persistence.store.readAllLogs(pagination).map(_.toList) })).leftMap(DatabaseError)
   }
   
   def authRetriever(implicit F0: MongoFormat[AuthorisationRecord]) = new MongoRetriever[AuthorisationRecord] {
     val F = F0
     val getCount = IO.sync(Persistence.store.collectionStats.get("auth").map(_.documentCount.toInt).getOrElse(0))
     def getItems(pagination: PaginationView) =
-      IO.flatten(IO.sync(IO.fromEither(Persistence.store.getAuthorisationList))).leftMap(DatabaseError)
+      IO.flatten(IO.sync(IO.fromEither { Persistence.store.getAuthorisationList(Some(pagination)).map(_.toList) })).leftMap(DatabaseError)
   }
   
   def apiKeyRetriever(implicit F0: MongoFormat[ApiKey]) = new MongoRetriever[ApiKey] {
     val F = F0
     val getCount = IO.sync(Persistence.store.collectionStats.get("apiKeys").map(_.documentCount.toInt).getOrElse(0))
     def getItems(pagination: PaginationView) =
-      IO.flatten(IO.sync(IO.fromEither(Persistence.store.getApiKeyList(None)))).leftMap(DatabaseError)
+      IO.flatten(IO.sync(IO.fromEither { Persistence.store.getApiKeyList(Some(pagination)).map(_.toList) })).leftMap(DatabaseError)
   }
 
 }
