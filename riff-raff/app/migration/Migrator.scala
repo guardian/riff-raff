b900577810986754736829efec6580ac70bb25df
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2019-02-02 18:10:53 +0000

    Fix queue reading

--- riff-raff/app/migration/Migrator.scala
+++ riff-raff/app/migration/Migrator.scala
@@ -20,36 +20,49 @@ trait Migrator[Response] {
   ): IO[MigrationError, (Ref[Int], Fiber[MigrationError, _], Fiber[MigrationError, List[Response]], Response, Response)] =
     for {
       // n is the maximum number of items we want to retrieve, by default the whole table
-      n <- limit.map(IO.now).getOrElse(retriever.getCount)
+      n <- retriever.getCount.map(max => limit.map(Math.min(max, _)).getOrElse(max))
+      window = Math.min(n, WINDOW_SIZE)
       cpt <- Ref(n)
       r1 <- dropTable(ToPostgres[A])
       r2 <- createTable(ToPostgres[A])
       // we're going to paginate through the results to reduce memory overhead
-      window = limit.map(Math.min(_, WINDOW_SIZE)).getOrElse(WINDOW_SIZE)
       q <- Queue.bounded[A](window)
       f1 <- retriever.getAllItems(q, window, n).fork
       f2 <- insertAllItems(q, cpt, window).fork
     } yield (cpt, f1, f2, r1, r2)
 
   def insertAllItems[A: ToPostgres](queue: Queue[A], counter: Ref[Int], window: Int): IO[MigrationError, List[Response]] = {
-    def loop(resps: List[Response]): IO[MigrationError, List[Response]] = for {
-      n <- counter.get
-      rs <- if (n <= 0)
-        IO.now(resps)
-      else
+    def loop(resps: List[Response]): IO[MigrationError, List[Response]] = {
+      def loop_in(n: Int): IO[MigrationError, List[A]] =
         queue.takeUpTo(window).flatMap { items =>
-          if (items.length == 0)
-            loop(resps)
+          if (items.length >= n)
+            IO.now(items.take(n))
+          else if (items.length == 0)
+            loop_in(n)
           else
+            loop_in(n - items.length).map(items ++ _)
+        }
+
+      for {
+        n <- counter.get
+        rs <- if (n <= 0)
+          IO.now(resps)
+        else {
+          val max = Math.min(n, window)
+          loop_in(max).flatMap { items =>
             IO.flatten(counter.modify { n => 
-              val op = if (items.length < window || n - items.length <= 0)
-                insertAll(ToPostgres[A], items).flatMap(r => IO.now(r :: resps))
-              else
-                insertAll(ToPostgres[A], items).flatMap(r => loop(r :: resps))
+              val op = insertAll(ToPostgres[A], items).flatMap { r => 
+                if (n - items.length <= 0)
+                  IO.now(r :: resps)
+                else
+                  loop(r :: resps)
+              }
               (op, n - items.length)
             })
+          }
         }
-    } yield rs
+      } yield rs
+    }
 
     loop(Nil)
   }
