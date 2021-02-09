68f26e5cc0752286db0e1b4c15d93f0cf6ffd4cd
Jacob Winch <jacob.winch@guardian.co.uk>
2021-02-09 15:33:05 +0000

    Add some unit tests

--- riff-raff/app/notification/FailureNotificationContents.scala
+++ riff-raff/app/notification/FailureNotificationContents.scala
@@ -27,31 +27,32 @@ object FailureNotificationContents {
     prefix + path.url
   }
 
+  def viewProblematicDeploy(uuid: UUID, status: String, urlPrefix: String) = Action(s"View $status deploy", problematicDeployUrl(urlPrefix, uuid))
+
   def deployFailedNotificationContents(uuid: UUID, parameters: DeployParameters, urlPrefix: String): NotificationContents = {
     NotificationContents(
       subject = s"${parameters.deployer.name} failed",
       message = s"${parameters.deployer.name} for ${parameters.build.projectName} (build ${parameters.build.id}) to stage ${parameters.stage.name} failed.",
-      actions = List(Action("View failed deploy", problematicDeployUrl(urlPrefix, uuid)))
+      actions = List(viewProblematicDeploy(uuid, "failed", urlPrefix))
     )
   }
 
   def deployUnstartedNotificationContents(scheduledDeployError: ScheduledDeployError, urlPrefix: String, teamTargetsSearch: (Option[UUID], Option[DeployParameters]) => List[Target], riffRaffTargets: List[Target]): NotificationContentsWithTargets = {
-    val subject = "Scheduled deploy failed to start"
-    def viewProblematicDeploy(uuid: UUID, status: String) = Action(s"View $status deploy", problematicDeployUrl(urlPrefix, uuid))
+    val subject = "Scheduled Deployment failed to start"
     scheduledDeployError match {
       case SkippedDueToPreviousFailure(record) =>
-        val message = s"A scheduled deploy of ${record.parameters.build.projectName} to ${record.parameters.stage} didn't start because the most recent deploy failed."
+        val message = s"Scheduled Deployment for ${record.parameters.build.projectName} to ${record.parameters.stage.name} didn't start because the most recent deploy failed."
         val redeployAction = Action("Redeploy manually", deployAgainUrl(urlPrefix, record.uuid))
-        val contents = NotificationContents(subject, message, List(viewProblematicDeploy(record.uuid, "failed"), redeployAction))
+        val contents = NotificationContents(subject, message, List(viewProblematicDeploy(record.uuid, "failed", urlPrefix), redeployAction))
         NotificationContentsWithTargets(contents, teamTargetsSearch(Some(record.uuid), Some(record.parameters)))
       case SkippedDueToPreviousWaitingDeploy(record) =>
-        val message = s"A scheduled deploy of ${record.parameters.build.projectName} to ${record.parameters.stage} failed to start as a previous deploy was still waiting to be deployed."
-        val contents = NotificationContents(subject, message, List(viewProblematicDeploy(record.uuid, "waiting")))
+        val message = s"Scheduled Deployment for ${record.parameters.build.projectName} to ${record.parameters.stage.name} failed to start as a previous deploy was still waiting to be deployed."
+        val contents = NotificationContents(subject, message, List(viewProblematicDeploy(record.uuid, "waiting", urlPrefix)))
         NotificationContentsWithTargets(contents, riffRaffTargets)
       case NoDeploysFoundForStage(projectName, stage) =>
-        val message = s"A scheduled deploy didn't start because RiffRaff has never deployed $projectName to $stage before. " +
+        val message = s"A scheduled deploy didn't start because Riff-Raff has never deployed $projectName to $stage before. " +
           "Please inform the owner of this schedule as it's likely that they have made a configuration error."
-        val scheduledDeployConfig = Action("View scheduled deploy configuration", scheduledDeployConfigUrl(urlPrefix))
+        val scheduledDeployConfig = Action("View Scheduled Deployment configuration", scheduledDeployConfigUrl(urlPrefix))
         val contents = NotificationContents(subject, message, List(scheduledDeployConfig))
         NotificationContentsWithTargets(contents, riffRaffTargets)
     }
---
+++ riff-raff/test/notification/FailureNotificationContentsTest.scala
@@ -0,0 +1,102 @@
+package notification
+
+import FailureNotificationContents._
+import ci.ContinuousDeployment
+import com.gu.anghammarad.models.{Action, Stack, Target}
+import deployment.{Fixtures, NoDeploysFoundForStage, SkippedDueToPreviousFailure, SkippedDueToPreviousWaitingDeploy}
+import magenta.{Build, DeployParameters, Stage}
+import org.scalatest.{FunSuite, Matchers}
+
+import java.util.UUID
+
+class FailureNotificationContentsTest extends FunSuite with Matchers {
+
+  test("should produce sensible notification contents for a failed Continuous Deployment") {
+    val uuid = UUID.randomUUID()
+    val parameters = DeployParameters(ContinuousDeployment.deployer, Build("project", "123"), Stage("PROD"))
+    val expected = NotificationContents(
+      subject = "Continuous Deployment failed",
+      message = "Continuous Deployment for project (build 123) to stage PROD failed.",
+      actions = List(Action("View failed deploy", s"http://localhost:9000/deployment/view/$uuid?verbose=true"))
+    )
+    val result = deployFailedNotificationContents(uuid, parameters, "http://localhost:9000")
+    result shouldBe expected
+  }
+
+  test("should produce sensible notification contents for a Scheduled Deployment which fails to start due to previous failed deploy") {
+    val record = Fixtures.createRecord(
+      projectName = "testProject",
+      buildId = "123",
+      stage = "PROD"
+    )
+    def fakeGetTargets(uuid: Option[UUID], maybeParameters: Option[DeployParameters]): List[Target] = List(Stack("another-team-stack"))
+    val error = SkippedDueToPreviousFailure(record)
+    val expectedContents = NotificationContents(
+      subject = "Scheduled Deployment failed to start",
+      message = "Scheduled Deployment for testProject to PROD didn't start because the most recent deploy failed.",
+      actions = List(
+        Action("View failed deploy", s"http://localhost:9000/deployment/view/${record.uuid}?verbose=true"),
+        Action("Redeploy manually", s"http://localhost:9000/deployment/deployAgain/${record.uuid}")
+      ),
+    )
+    val result = deployUnstartedNotificationContents(error, "http://localhost:9000", fakeGetTargets, List(Stack("deploy")))
+    result.notificationContents shouldBe expectedContents
+  }
+
+  test("should produce sensible notification contents for a Scheduled Deployment which fails to start due to previous waiting deploy") {
+    val record = Fixtures.createRecord(
+      projectName = "testProject",
+      buildId = "123",
+      stage = "PROD"
+    )
+    def fakeGetTargets(uuid: Option[UUID], maybeParameters: Option[DeployParameters]): List[Target] = List(Stack("another-team-stack"))
+    val error = SkippedDueToPreviousWaitingDeploy(record)
+    val expectedContents = NotificationContents(
+      subject = "Scheduled Deployment failed to start",
+      message = "Scheduled Deployment for testProject to PROD failed to start as a previous deploy was still waiting to be deployed.",
+      actions = List(Action("View waiting deploy", s"http://localhost:9000/deployment/view/${record.uuid}?verbose=true")),
+    )
+    val result = deployUnstartedNotificationContents(error, "http://localhost:9000", fakeGetTargets, List(Stack("deploy")))
+    result.notificationContents shouldBe expectedContents
+  }
+
+  test("should produce sensible notification contents for a Scheduled Deployment which fails to start due to a configuration issue") {
+    def fakeGetTargets(uuid: Option[UUID], maybeParameters: Option[DeployParameters]): List[Target] = List(Stack("another-team-stack"))
+    val error = NoDeploysFoundForStage("testProject", "PROD")
+    val expectedContents = NotificationContents(
+      subject = "Scheduled Deployment failed to start",
+      message = "A scheduled deploy didn't start because Riff-Raff has never deployed testProject to PROD before. Please inform the owner of this schedule as it's likely that they have made a configuration error.",
+      actions = List(Action("View Scheduled Deployment configuration", s"http://localhost:9000/deployment/schedule"))
+    )
+    val result = deployUnstartedNotificationContents(error, "http://localhost:9000", fakeGetTargets, List(Stack("deploy")))
+    result.notificationContents shouldBe expectedContents
+  }
+
+  test("should use team-based targets for a Scheduled Deployment which fails to start due to previous failed deploy") {
+    val record = Fixtures.createRecord()
+    def fakeGetTargets(uuid: Option[UUID], maybeParameters: Option[DeployParameters]): List[Target] = List(Stack("another-team-stack"))
+    val error = SkippedDueToPreviousFailure(record)
+    val expectedTargets = List(Stack("another-team-stack"))
+    val result = deployUnstartedNotificationContents(error, "http://localhost:9000", fakeGetTargets, List(Stack("deploy")))
+    result.targets shouldBe expectedTargets
+  }
+
+  test("should use Riff-Raff maintainer targets for a Scheduled Deployment which fails to start due to previous waiting deploy") {
+    val record = Fixtures.createRecord()
+    def fakeGetTargets(uuid: Option[UUID], maybeParameters: Option[DeployParameters]): List[Target] = List(Stack("another-team-stack"))
+    val error = SkippedDueToPreviousWaitingDeploy(record)
+    val expectedTargets = List(Stack("deploy"))
+    val result = deployUnstartedNotificationContents(error, "http://localhost:9000", fakeGetTargets, List(Stack("deploy")))
+    result.targets shouldBe expectedTargets
+  }
+
+  test("should use Riff-Raff maintainer targets for a Scheduled Deployment which fails to start due to a configuration issue") {
+    val record = Fixtures.createRecord()
+    def fakeGetTargets(uuid: Option[UUID], maybeParameters: Option[DeployParameters]): List[Target] = List(Stack("another-team-stack"))
+    val error = NoDeploysFoundForStage("project", "PROD")
+    val expectedTargets = List(Stack("deploy"))
+    val result = deployUnstartedNotificationContents(error, "http://localhost:9000", fakeGetTargets, List(Stack("deploy")))
+    result.targets shouldBe expectedTargets
+  }
+
+}
