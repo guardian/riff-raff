4cd1f5cd7ccf5857d1dcf6eaac11ff64ba206ee2
Akash Askoolum <akash1810@gmail.com>
2021-08-11 08:51:38 +0100

    Revert "chore: Add `allowlist` field to DynamoDb"
    This reverts commit 3fdaa817a9de77a7d252e071d3eaef8d91c38ebf.

--- riff-raff/app/persistence/RestrictionConfigDynamoRepository.scala
+++ riff-raff/app/persistence/RestrictionConfigDynamoRepository.scala
@@ -1,13 +1,13 @@
 package persistence
 
 import java.util.UUID
+
 import com.gu.scanamo.Table
 import restrictions.{RestrictionConfig, RestrictionsConfigRepository}
 import cats.syntax.either._
-import com.gu.management.Loggable
 import conf.Config
 
-class RestrictionConfigDynamoRepository(config: Config) extends DynamoRepository(config) with RestrictionsConfigRepository with Loggable {
+class RestrictionConfigDynamoRepository(config: Config) extends DynamoRepository(config) with RestrictionsConfigRepository {
   def tablePrefix = "riffraff-restriction-config"
 
   val table = Table[RestrictionConfig](tableName)
@@ -17,24 +17,7 @@ class RestrictionConfigDynamoRepository(config: Config) extends DynamoRepository
   def setRestriction(config: RestrictionConfig): Unit = exec(table.put(config))
   def getRestriction(id: UUID): Option[RestrictionConfig] = exec(table.get('id -> id)).flatMap(_.toOption)
   def deleteRestriction(id: UUID): Unit = exec(table.delete('id -> id))
-  def getRestrictionList: Iterable[RestrictionConfig] = {
-    val restrictions = exec(table.scan()).flatMap(_.toOption)
-
-    /*
-    Renaming whitelist to allowlist is a three step dance:
-      1. Add the new field
-      2. Read the new field
-      3. Remove the old field
-
-     This is step 1.
-     */
-    restrictions.map(restriction => {
-      logger.info(s"adding new allowlist field to restriction ${restriction.id}")
-      exec(table.update('id -> restriction.id, set('allowlist -> restriction.whitelist)))
-    })
-
-    restrictions
-  }
+  def getRestrictionList: Iterable[RestrictionConfig] = exec(table.scan()).flatMap(_.toOption)
 
   def getRestrictions(projectName: String): Seq[RestrictionConfig] =
     exec(table.index("restriction-config-projectName").query('projectName -> projectName)).flatMap(_.toOption)
