241a6cf99c59ca935c72a53a0af7394e88191932
Tom Wey <tom.wey@theguardian.com>
2021-08-27 13:51:22 +0100

    When linking, handle https git URLs with .git ext
    I noticed that the repo URL for frontend is
    https://github.com/guardian/frontend.git. Afaict this works fine
    everywhere except where we build a link to a commit from the
    deployment/view page where the link to the commit is broken due to a
    superflous .git in the repo path.

--- riff-raff/test/utils/VCSInfoTest.scala
+++ riff-raff/test/utils/VCSInfoTest.scala
@@ -15,4 +15,9 @@ class VCSInfoTest extends AnyFunSuite with Matchers {
     val info = VCSInfo("git@github.com:guardian/contributions-frontend.git", "98a1cffadbe564d570b15c2113c07a28cbe835ee")
     info.get.baseUrl shouldBe new URI("https://github.com/guardian/contributions-frontend")
   }
+
+  test("extracts details from Github HTTPS URL with .git extension") {
+    val info = VCSInfo("https://github.com/guardian/contributions-frontend.git", "98a1cffadbe564d570b15c2113c07a28cbe835ee")
+    info.get.baseUrl shouldBe new URI("https://github.com/guardian/contributions-frontend")
+  }
 }
