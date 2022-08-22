43203e5972b3969f5ec00f93bfcf8dd2aed47323
Jon Soul <jonathan.soul@guardian.co.uk>
2022-08-22 14:55:31 +0100

    Revert "Update scanamo to 1.0.0-M20 in dependency-updates (#773)"
    This reverts commit 2eaabf036cc7ed33b71795e164fd5bc075006133.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -70,7 +70,7 @@ object Dependencies {
     "com.typesafe.akka" %% "akka-agent" % "2.5.32",
     "org.pegdown" % "pegdown" % "1.6.0",
     "com.adrianhurt" %% "play-bootstrap" % "1.6.1-P28-B3", // scala-steward:off,
-    "org.scanamo" %% "scanamo" % "1.0.0-M20",
+    "org.scanamo" %% "scanamo" % "1.0.0-M11",
     "software.amazon.awssdk" % "dynamodb" % Versions.aws,
     "software.amazon.awssdk" % "sns" % Versions.aws,
     "org.quartz-scheduler" % "quartz" % "2.3.2",
