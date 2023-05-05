3015198c758b17d7f96a0307b0af5529f62b5564
Mario Savarese <mario.savarese@guardian.co.uk>
2023-05-05 19:24:13 +0100

    Deduplicate code

--- magenta-lib/src/main/scala/magenta/tasks/FastlyTasks.scala
+++ magenta-lib/src/main/scala/magenta/tasks/FastlyTasks.scala
@@ -1,37 +1,34 @@
 package magenta.tasks
 
 import java.util.concurrent.Executors
-
 import com.gu.fastly.api.FastlyApiClient
 import magenta._
 import magenta.artifact.S3Path
-import play.api.libs.json.{JsString, Json}
+import play.api.libs.json.{JsString, Json, Reads}
 import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.s3.model.GetObjectRequest
 
-import scala.concurrent.duration._
-import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutorService}
 
 case class Version(number: Int, active: Option[Boolean])
 object Version {
-  implicit val reads = Json.reads[Version]
+  implicit val reads: Reads[Version] = Json.reads[Version]
 }
 
 case class Vcl(name: String)
 object Vcl {
-  implicit val reads = Json.reads[Vcl]
+  implicit val reads: Reads[Vcl] = Json.reads[Vcl]
 }
 
-case class UpdateFastlyConfig(s3Package: S3Path)(implicit
+case class UpdateFastlyConfigUtils(s3Package: S3Path)(implicit
     val keyRing: KeyRing,
-    artifactClient: S3Client
-) extends Task {
+    artifactClient: S3Client,
+    parameters: DeployParameters
+) extends Task
+    with FastlyUtils {
 
-  implicit val ec =
+  implicit val ec: ExecutionContextExecutorService =
     ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(10))
-
-  // No, I'm not happy about this, but it gets things working until we can make a larger change
-  def block[T](f: => Future[T]) = Await.result(f, 1.minute)
 
   override def execute(
       resources: DeploymentResources,
@@ -57,45 +54,19 @@ case class UpdateFastlyConfig(s3Package: S3Path)(implicit
         resources.reporter,
         stopFlag
       )
+
+      commentVersion(
+        nextVersionNumber,
+        client,
+        resources.reporter,
+        parameters,
+        stopFlag
+      )
+
       activateVersion(nextVersionNumber, client, resources.reporter, stopFlag)
 
       resources.reporter
         .info(s"Fastly version $nextVersionNumber is now active")
-    }
-  }
-
-  def stopOnFlag[T](stopFlag: => Boolean)(block: => T): T =
-    if (!stopFlag) block
-    else
-      throw new DeployStoppedException(
-        "Deploy manually stopped during UpdateFastlyConfig"
-      )
-
-  private def getActiveVersionNumber(
-      client: FastlyApiClient,
-      reporter: DeployReporter,
-      stopFlag: => Boolean
-  ): Int = {
-    stopOnFlag(stopFlag) {
-      val versionList = block(client.versionList())
-      val versions = Json.parse(versionList.getResponseBody).as[List[Version]]
-      val activeVersion = versions.filter(x => x.active.getOrElse(false))(0)
-      reporter.info(s"Current active version ${activeVersion.number}")
-      activeVersion.number
-    }
-  }
-
-  private def clone(
-      versionNumber: Int,
-      client: FastlyApiClient,
-      reporter: DeployReporter,
-      stopFlag: => Boolean
-  ): Int = {
-    stopOnFlag(stopFlag) {
-      val cloned = block(client.versionClone(versionNumber))
-      val clonedVersion = Json.parse(cloned.getResponseBody).as[Version]
-      reporter.info(s"Cloned version ${clonedVersion.number}")
-      clonedVersion.number
     }
   }
 
@@ -192,7 +163,7 @@ object FastlyApiClientProvider {
       case credentials: ApiStaticCredentials =>
         val serviceId = credentials.id
         val apiKey = credentials.secret
-        new FastlyApiClient(apiKey, serviceId)
+        FastlyApiClient(apiKey, serviceId)
     }
   }
 }
