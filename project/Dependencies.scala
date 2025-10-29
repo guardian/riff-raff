5cb7c344622eb51bec7aa2be8117432c6809bcb6
Jorge Azevedo <jorge.azevedo@guardian.co.uk>
2025-10-29 16:43:01 +0000

    Revert quartz back to original version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -71,7 +71,7 @@ object Dependencies {
       "org.scanamo" %% "scanamo" % "4.0.0",
       "software.amazon.awssdk" % "dynamodb" % Versions.aws,
       "software.amazon.awssdk" % "sns" % Versions.aws,
-      "org.quartz-scheduler" % "quartz" % "2.5.1",
+      "org.quartz-scheduler" % "quartz" % "2.3.2",
       "com.gu" %% "anghammarad-client" % "6.0.0",
       "org.webjars" %% "webjars-play" % "3.0.9",
       "org.webjars" % "jquery" % "3.7.1",
